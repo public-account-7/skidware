792817f0d7f58baf25b1cbdcc1385f6fce75bd2d
public-account-7 <doitenroimain@gmail.com>
2025-09-05 21:59:54 +0700

    Update games.lua

--- games.lua
+++ games.lua
@@ -1,5 +1,7 @@
 return {
     [1639994361] = {"Mouse.Hit"},
     [7942799470] = {"Mouse.Hit", "Remote"},
-    [1008451066] = {"Mouse.Hit", "Raycast"}
+    [1008451066] = {"Mouse.Hit", "Raycast"},
+    [8577897816] = {"Mouse.Hit", "Raycast"},
+    [8227131888] = {"Mouse.Hit", "Remote"}
 }
