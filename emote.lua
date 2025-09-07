a118ee3326a507ed204acdba2398a93d6fd54f34
public-account-7 <doitenroimain@gmail.com>
2025-09-07 16:40:35 +0700

    Update emote.lua

--- emote.lua
+++ emote.lua
@@ -1,1 +1,1 @@
-return {Floss = "rbxassetid://10714340543"}
+return {Floss = "rbxassetid://10714340543", ["Cool Dance"] = "rbxassetid://15609995579", ["L Dance"] = "rbxassetid://127750948928504", SideKicks = "rbxassetid://10370362157"}
