92043cd482d1763f09ad1631b1424f8fdfbf618e
public-account-7 <doitenroimain@gmail.com>
2025-10-15 19:22:39 +0700

    Delete games.lua

--- games.lua
+++
@@ -1,8 +0,0 @@
-return {
-    [1639994361] = {"Mouse.Hit"},
-    [7942799470] = {"Mouse.Hit", "Remote"},
-    [1008451066] = {"Mouse.Hit", "Raycast"},
-    [8577897816] = {"Mouse.Hit", "Raycast"},
-    [8227131888] = {"Mouse.Hit", "Remote"},
-    [8570227859] = {"Mouse.Hit", "Raycast"}
-}
