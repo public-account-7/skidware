20d6b05ed0195db1e8292968cece9bf08da671d4
public-account-7 <doitenroimain@gmail.com>
2025-11-23 17:47:53 +0700

    Update sdk.lua

--- sdk.lua
+++ sdk.lua
@@ -29,7 +29,7 @@ LPH_JIT = function(f, ...)
     return f
 end
 LPH_JIT_MAX = LPH_JIT
-
+MV_NO_OP = LPH_JIT
 LPH_NO_VIRTUALIZE = function(f, ...)
     assert(type(f) == "function" and #{...} == 0, "LPH_NO_VIRTUALIZE only accepts a single constant function as an argument.")
     return f
