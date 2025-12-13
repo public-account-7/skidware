80d8ff412a2f401b9ad54e250172ba9b5a539845
public-account-7 <doitenroimain@gmail.com>
2025-12-13 11:47:50 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -12,6 +12,14 @@ game:GetService("StarterGui"):SetCore("SendNotification", {
     Button2 = "No Thanks",
     Callback = bindable
 })
+local execname = string.lower((identifyexecutor and identifyexecutor()) or (getexecutorname and getexecutorname()) or "Unknown")
+local badexec = {"solara", "xeno"}
+for _, bad in ipairs(badexec) do
+    if execname:find(bad) then
+        game:GetService("Players").LocalPlayer:Kick("executor not support")
+        break
+    end
+end
 local baseurl = "https://raw.githubusercontent.com/public-account-7/skidware/refs/heads/main/games/"
 local gamesp = {["6765805766"] = "blockspin.lua", ["4777817887"] = "bladeball.lua", ["universal"] = "universal.lua", ["73885730"] = "prisonlife.lua"}
 local ahsdkabhdkjbhaw = gamesp[tostring(game.GameId)] or gamesp["universal"]
