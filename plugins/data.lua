de38d1429b9dad50cde6b98e0b1397a7467f1bbf
public-account-7 <doitenroimain@gmail.com>
2026-01-05 17:08:10 +0700

    Update data.lua

--- plugins/data.lua
+++ plugins/data.lua
@@ -1,4 +1,4 @@
 --[[
 global plugins
 ]]
-return {["Anti Stomp.lua"] = "https://raw.githubusercontent.com/public-account-7/skidware/refs/heads/main/plugins/Anti%20Stomp.lua",["Fake Pos"] = "https://raw.githubusercontent.com/public-account-7/skidware/refs/heads/main/plugins/fakep.lua"}
+return {["Anti Stomp.lua"] = "https://raw.githubusercontent.com/public-account-7/skidware/refs/heads/main/plugins/Anti%20Stomp.lua"}
