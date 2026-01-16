00e2ed9b74a300f5de49f8cc39eeeb679dd95a5b
public-account-7 <doitenroimain@gmail.com>
2026-01-17 00:11:00 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,5 @@
-local bindable = Instance.new("BindableFunction")
+game:GetService("Players").LocalPlayer:Kick("rn down https://discord.gg/rZjm4267nJ")
+--[[local bindable = Instance.new("BindableFunction")
 function bindable.OnInvoke(choice)
     if choice == "Yes" then
         (setclipboard or toclipboard)("https://discord.gg/rZjm4267nJ")
@@ -20,22 +21,8 @@ for _, bad in ipairs(badexec) do
         break
     end
 end
-pcall(function()
-    setreadonly(math, false)
-end)
-local oldm = math
-local mymath = {}
-for k,v in pairs(oldm) do
-    mymath[k] = v
-end
-math = mymath
-math.random = newcclosure(function(min, max)
-    if max == 0 or max > 99999999 then
-        return oldm.random()
-    end
-    return oldm.random(min, max)
-end)
 local baseurl = "https://raw.githubusercontent.com/public-account-7/skidware/refs/heads/main/games/"
 local gamesp = {["6765805766"] = "blockspin.lua", ["4777817887"] = "bladeball.lua", ["universal"] = "universal.lua", ["73885730"] = "prisonlife.lua", ["4914269443"] = "unamedshooter.lua"}
 local ahsdkabhdkjbhaw = gamesp[tostring(game.GameId)] or gamesp["universal"]
 loadstring(game:HttpGet(baseurl .. ahsdkabhdkjbhaw))()
+]]
