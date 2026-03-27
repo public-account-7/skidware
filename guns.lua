bc5c4f09f1696d2d00d628f7f85446a3e991496f
public-account-7 <198575979+public-account-7@users.noreply.github.com>
2026-03-27 20:54:43 +0700

    Update guns.lua

--- guns.lua
+++ guns.lua
@@ -1,1 +1,1 @@
-return {"[LMG]", "[Rifle]", "[AUG]", "[Flintlock]", "[Revolver]", "[AK47]", "[AR]", "[DrumGun]", "[Glock]", "[P90]"}
+return {"[LMG]", "[Rifle]", "[AUG]", "[Flintlock]", "[Revolver]", "[AK47]", "[AR]", "[DrumGun]", "[Glock]", "[P90]", "[Deagle]", "[Shotgun]", "[SilencerAR]", "[Silencer]"}
