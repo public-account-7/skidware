03002ef2d5161ad08a043ad399a3e649e0c18d16
public-account-7 <198575979+public-account-7@users.noreply.github.com>
2026-03-27 21:41:30 +0700

    Update library.lua

--- library.lua
+++ library.lua
@@ -40,7 +40,7 @@ local Library = {
     RiskColor = Color3.fromRGB(255, 50, 50),
 
     Black = Color3.new(0, 0, 0);
-    Font = Enum.Font.Gotham,
+    Font = Enum.Font.SourceSansBold,
 
     OpenedFrames = {};
     DependencyBoxes = {};
