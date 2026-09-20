b38669c5ee0dc3ac57f41b5233c15092455390de
public-account-7 <doitenroimain@gmail.com>
2026-09-20 10:57:26 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,7 +1,7 @@
 repeat
     task.wait()
 until game:IsLoaded()
-
+pcall(delfolder, "polsec")
 
 if getgenv().omgthisshitisloadedlmao then return warn("skidware already loaded") end
 getgenv().omgthisshitisloadedlmao = true
